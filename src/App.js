6fc3e09e31977df4cfd0953124290c31a3c61845
Max <nikulins.maksims@gmail.com>
2024-11-22 09:59:43 +0200

    need checkout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
-
 function App() {
+  
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then((response) => response.json())
+    .then((json) => setUsers(json.data))
+    .catch((error) => {
+      console.log(error);
+      alert('Error users response data')
+    })
+  }, [])
+  
   return (
     <div className="App">
       <Users />
